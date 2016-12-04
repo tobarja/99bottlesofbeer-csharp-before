108be72d19458184a197f59ec1acd018b244fc4a
Andrew Thompson <andrew@tobarja.com>
2016-12-03 20:05:58 -0500

    Add project files.

---
+++ 99BottlesOfBeer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99BottlesOfBeer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
