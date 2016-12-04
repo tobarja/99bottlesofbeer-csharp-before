382a4b8b3f40fa92761a97c2eaa50038434547a9
Andrew Thompson <andrew@tobarja.com>
2016-12-03 21:45:40 -0500

    99 Bottles of Beer w/ Tests

--- 99BottlesOfBeer/Program.cs
+++ 99BottlesOfBeer/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _99BottlesOfBeer
 {
@@ -10,6 +6,8 @@ namespace _99BottlesOfBeer
     {
         static void Main(string[] args)
         {
+            var beerSong = new Song();
+            Console.WriteLine(beerSong.Sing());
         }
     }
 }
---
+++ 99BottlesOfBeer/Song.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace _99BottlesOfBeer
+{
+    public class Song
+    {
+        public Song()
+        {
+        }
+
+        public string Sing()
+        {
+            var song = new StringBuilder();
+            for (var bottlesRemaining = 99; bottlesRemaining >= 0; bottlesRemaining--)
+            {
+                song.Append(VerseFor(bottlesRemaining));
+            }
+            return song.ToString();
+        }
+
+        public string VerseFor(int v)
+        {
+            switch (v)
+            {
+                case 2:
+                    return $@"2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+";
+
+                case 1:
+                    return $@"1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+";
+
+                case 0:
+                    return $@"No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+";
+
+                default:
+                    return $@"{v} bottles of beer on the wall, {v} bottles of beer.
+Take one down and pass it around, {v - 1} bottles of beer on the wall.
+";
+            }
+        }
+    }
+}
